95aa8e4a0ac778127ae4a404e5e2cd265b0ec753
Shaira Juezan <juezanshaira18@gmail.com>
2026-09-24 06:48:10 +0800

    Fix home route

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
 
 // =========================
 // MONGODB
